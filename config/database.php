4e2ccea099e1f09153a17c1e8ddaa3d7f94bec62
Alex Calvin GBETIE <calvex.gbetie@gmail.com>
2019-02-19 13:54:23 +0000

    parametres base de donnees

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql40.main-hosting.eu.'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'u842547335_sta'),
+            'username' => env('DB_USERNAME', 'u842547335_sta'),
+            'password' => env('DB_PASSWORD', '@]K:WD9@c+`P'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
